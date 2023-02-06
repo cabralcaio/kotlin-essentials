748f9ce9f8d5c37297ec6c623cb8bc6943a46d4a
cabralcaio <caio_cabral1@hotmail.com>
2023-02-06 17:13:33 -0300

    Variables (var and val)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
-fun main () {
+fun main(args: Array<String>) {
     println("Hello, World! I'm Caio.")
+
+    val a = 26
+    var b = "ABC"
+    // Kotlin is smart enough to infer the type from the value that is set.
+
+
+    println(a) // the result must be 26
+    println(b) // the result must be ABC (string)
+    // a = 23 it's not possible because a cannot be reassigned as I started using val (value not variable)
+    b = "CDE"
+    println(b) // CDE
+
+    val f: Int // declare without initialize (must be avoided)
+    f = 2
 }
